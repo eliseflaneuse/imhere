33a9aeab304fcf64e1f37bb248c46db6b54663d9
Elise Flaneuse <eliseflaneuse@gmail.com>
2024-03-22 11:32:12 -0300

    input and handleParticipantsAdd function on Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
+  function handleParticipantAdd() {
+    console.log('Você clicou no botão de Adicionar')
+  }
   return (
     <View style={styles.container}>
       <Text key="name" style={styles.eventName}>
@@ -11,11 +14,14 @@ export function Home() {
 
       <Text style={styles.eventDate}>Sexta, 22 de Março de 2024</Text>
 
-      <TextInput 
-      style={styles.input} 
-      placeholder="Nome do participante"
-      placeholderTextColor='#6B6B6B'
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
       />
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -26,5 +26,17 @@ export const styles = StyleSheet.create({
     padding: 16,
     fontSize: 20,
     marginTop: 12,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 24,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31cf67',
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
